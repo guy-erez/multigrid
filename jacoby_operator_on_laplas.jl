87ab21ddc51ac50d2fdf11fe0e67755435b270bc
guy-erez <guy.e12@gmail.com>
2019-02-28 12:36:39 +0200

    finish writing jacobi TODO : document and test (add ploting of the error)

--- jacoby_operator_on_laplas.jl
+++ jacoby_operator_on_laplas.jl
@@ -1,3 +1,4 @@
+using LinearAlgebra
 #=
 PURPOSE : preform an approximation to the laplasian of u at possition u0.
 INPUT : u0 - the possition where the laplasian is approximated.
@@ -5,20 +6,29 @@ INPUT : u0 - the possition where the laplasian is approximated.
 		u_pi = u0 + hi
 		where i denote the directions 1,2
 		hi - the grid spaceing in the i directions
+		w,f are not used in this
 OUTPUT : the value of the approximation to the laplasian at u0.
 =#
 
-function Apply2DLaplasian(u0::Float64,u_m1::Float64,u_p1::Float64,u_m2::Float64,u_p2::Float64,h1::Float64,h2::Float64) # x-->u
+function Apply2DLaplasian(u0::Float64,u_m1::Float64,u_p1::Float64,u_m2::Float64,u_p2::Float64,h1::Float64,h2::Float64,w,f) # x-->u
 	h1invsq = 1.0/(h1^2)
 	h2invsq = 1.0/(h2^2)
 	return (2*h1invsq + 2*h2invsq)*u0 - h1invsq*(u_m1 + u_p1) - h2invsq*(u_m2 + u_p2); # wrong sign? d^2f/du^2 = (f(x0-h) - 2f(x0) + f(x0+h)) / h^2
 end
 
 
-function Apply2DJacobi(u0::Float64,u_m1::Float64,u_p1::Float64,u_m2::Float64,u_p2::Float64,h1::Float64,h2::Float64,w::Float64,f::Float64)
+function Apply2DJacobiStep(u0::Float64,u_m1::Float64,u_p1::Float64,u_m2::Float64,u_p2::Float64,h1::Float64,h2::Float64,w::Float64,f::Float64)
 	d = -2*(1.0/(h1^2) + 1.0/(h2^2)) 								# the diagonal element of the laplasian operator
 	L_u = Apply2DLaplasian(u0,u_m1,u_p1,u_m2,u_p2,h1,h2)			# laplasian*u
 	return u0 + w*(1.0/d)*(f - L_u)
+end
+
+
+function jacobi!(n, h, u::Array, y::Array, w::Float64, f::Array, max_iter ::Int , tolerance ::Float64 ,boundry_condition :: Function)
+	for i in 1:max_iter
+		boundry_condition(n,h,u,y,w,f,Apply2DJacobiStep)
+		u = y
+	end
 end
 
 
@@ -65,15 +75,19 @@ end
 end
 
 
-
-
 n = [10,10];
 h = 1.0./n;
 u = randn(tuple((n.-1)...))
-y = randn(tuple((n.-1)...))
+output_jacobi = randn(tuple((n.-1)...))
+output_laplasian = randn(tuple((n.-1)...))
 f = randn(tuple((n.-1)...))
 w = 2.0/3.0
 
-multOpDirichlet!(n,h,u,y,w,f,Apply2DJacobi);
+jacobi!(n,h,u,output_jacobi,w,f,50, 10^-10,multOpDirichlet!)
+multOpDirichlet!(n,h,output_jacobi,output_laplasian,w,f,Apply2DLaplasian)   # laplasian on the output of jacoi into "output_laplasian"
+residual = output_laplasian - f
 
-println(y)
+println("\nthe approximate value is : $y and the abs of the residual norm is $(norm(residual))")
+
+
+# i dont understand
